855e4676c50cdebf1afa4811b1eff8bf2d3d96ad
Joakim-Andersson <jjoakimm@hotmail.com>
2021-01-28 14:46:21 +0100

    Mobile first approach to about-page

--- pages/about.js
+++ pages/about.js
@@ -5,27 +5,25 @@ export default function About() {
     return (
         <section className="about">
             <div className="about__text">
-                <div className="giant-quotes-quote">
-                    <blockquote>
-                        <h3 >I enjoy building apps, solving problems and learning new technologies along the way.
-                        </h3>
-                    </blockquote>
-                    <h2>Hey, I'm Joakim!</h2>
-                    <p>
-                        Full-stack developer with a background in Digital Marketing and E-commerce.
-                        I’m passionate and digitally talented with a natural ability to get things done.
+                <blockquote>
+                    <h2 >I enjoy building apps, solving problems and learning new technologies along the way.
+                        </h2>
+                </blockquote>
+                <h3>Hey, I'm Joakim!</h3>
+                <p>
+                    Full-stack developer with a background in Digital Marketing and E-commerce.
+                    I’m passionate and digitally talented with a natural ability to get things done.
+                    </p>
+                <p>
+                    Based in Oslo, Norway, but originally from Sweden. Right now working as a consultant at <a href="https://salt.study/" target="_blank"> {`<SALT/>`} </a> School of Applied Technology.
+                            Among 1500 candidates, I was one of 30 to be selected for a 3 months accelerated career program to become a Full Stack JavaScript developer.
+                            Now I am eager to make a difference in this industry!
                         </p>
-                    <p>
-                        Based in Oslo, Norway, but originally from Sweden. Right now working as a consultant at <a href="https://salt.study/"> {`<SALT/>`} </a>, School of Advannced Technology.
-                            Among 1500 candidates, myself and 30 others, were selected for a 3 months accelerated career program to become a Full Stack JavaScript developer.
-                            Now I am looking forward to making a difference in this industry!
+                <Link href="/contact">
+                    <p className="text__end">
+                        Would you like to work with me? Fun, let's chat! &rarr;
                         </p>
-                    <Link href="/contact">
-                        <p className="text__end">
-                            Would you like to work with me? Fun, let's chat! &rarr;
-                        </p>
-                    </Link>
-                </div>
+                </Link>
             </div>
             <div className="about__image">
                 <Image
