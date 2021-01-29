67b44ef83edace0f1a30f511c1eecaf585956619
Joakim-Andersson <jjoakimm@hotmail.com>
2021-01-29 10:51:41 +0100

    Update main page styling

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Image from "next/image"
 import styles from '../styles/Home.module.css'
 
 
@@ -10,11 +10,19 @@ export default function Home() {
       <div className="big"></div>
       <main className={styles.main__header}>
         <div className="main__header">
-          <h1>Fullstack developer</h1>
-          <h3>Joakim Andersson</h3>
+          <h1 className={styles.main__header__first}>Hey there, I'm <br/>
+            <span className={styles.main__header__name} > J</span>oakim
+          </h1>
+          <p className={styles.main__header__p}>Welcome to my portfolio </p>
+          <div className={styles.main__arrow}>
+                <Image className={styles.main_arrow_img}
+                    src="/down-arrow.png"
+                    alt="Linkedin Icon"
+                    width={70}
+                    height={70}
+                />
+            </div>
         </div>
-
-        
       </main>
     </div>
   )
