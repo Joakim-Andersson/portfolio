8e6a62e93d2b7ea6f020851830538fdab0955ae3
Joakim-Andersson <jjoakimm@hotmail.com>
2021-02-01 19:55:25 +0100

    Start work-component

--- components/work.js
+++ components/work.js
@@ -3,11 +3,20 @@ import Image from "next/image"
 export default function Work() {
     return (
         <section id="work" className="work">
-            <div className="work__text">
-                <h1>Project A</h1>
-                <h1>Project B</h1>
-                <h1>Project C</h1>
-            </div>
+            <ul className="work__projects">
+                <li className="work__projects-li">
+                        <Image
+                            src="/seasonalprod.jpg"
+                            alt="button for closing the menu"
+                            width={800}
+                            height={600}
+                            layout="intrinsic"
+
+                        />
+                        <p>text </p>
+                        <div className="colored-circle"></div>
+                </li>
+            </ul>
         </section>
     )
 }
